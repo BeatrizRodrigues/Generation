2577419e4adefee944d43dcef28f5ce580fca222
Beatriz Aparecida de Freitas Rodrigues <beeatrizz29@gmail.com>
2021-07-21 19:41:43 -0300

    Atividade bootstrap

---
+++ Spring/ListaDeAtividade1/hello2/src/main/java/com/helloworld2/hello2/controller/Hello2Controller.java
@@ -0,0 +1,15 @@
+package com.helloworld2.hello2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello2")
+public class Hello2Controller {
+	@GetMapping
+	public String hello2() {
+		return "Hello World!!!!\n Objetivo de aprendizagem: Spring-boot";
+		
+	}
+}
--- moduloTres/segundaAtividade/assets/js/script.js
+++ moduloTres/segundaAtividade/assets/js/script.js
@@ -1,3 +1,11 @@
+/*
+por Tag: getElementByTagName()
+por Id: getElementById()
+por Nome: getElementsByName()
+por Classe: getElementsByClassName()
+por Seletor: querySelector()
+*/
+
 const nav = document.getElementById('nav');
 window.addEventListener('scroll', () => {
     if (window.scrollY >= 100) {
@@ -6,3 +14,55 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('navBlack');
     }
 });
+
+let nome = window.document.getElementById('nome');
+let email = window.document.getElementById('email');
+let assunto = window.document.getElementById('assunto');
+let nomeOk = false;
+let emailOk = false;
+let assuntoOk = false;
+
+function validaNome(){
+    let txtNome = document.querySelector('#txtNome');
+    if(nome.value.length < 3){
+        txtNome.innerHTML = "Nome inválido";
+        txtNome.style.color = 'red';
+    }else{
+        txtNome.innerHTML = "Nome válido";
+        txtNome.style.color = 'green';
+        nomeOk = true;
+    }
+}
+
+function validaEmail(){
+    let txtEmail = document.querySelector('#txtEmail');
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){
+        txtEmail.innerHTML = "E-mail inválido";
+        txtEmail.style.color = 'red';
+    }else{
+        txtEmail.innerHTML = "E-mail válido";
+        txtEmail.style.color = 'green';
+        emailOk = true;
+    }
+}
+
+function validaAssunto(){
+    let txtAssunto = document.querySelector('#txtAssunto');
+    if(assunto.value.length >= 150){
+        txtAssunto.innerHTML = "Texto muito grande, digite no máximo 150 caracteres ";
+        txtAssunto.style.color = 'red';
+    }else{
+        txtAssunto.innerHTML = "Texto válido";
+        txtAssunto.style.color = 'green';
+        assuntoOk = true;
+    }
+}
+
+function enviar(){
+    if(nomeOk == true && emailOk == true && assuntoOk == true){
+        alert('Enviado com sucesso! :)');
+    }else{
+        alert('Preencha corretamente os campos antes de envir! :(');
+    }
+}
+
